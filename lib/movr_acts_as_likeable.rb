9a5c78882610ecd8554debfdceae27819bc886a0
Daryl Moulder <dmoulder@qtome.com>
2012-03-22 14:59:31 +1100

    use alex version

--- lib/movr_acts_as_likeable.rb
+++ lib/movr_acts_as_likeable.rb
@@ -1,68 +1,50 @@
-# MovrActsAsLikeable
-module ActiveRecord
-  module Acts
-    module Likeable
-
-      def self.included(base)
-        base.extend ClassMethods
-      end
-
-      module ClassMethods
-        def acts_as_likeable
-          has_many :likings, :as => :likeable, :dependent => :destroy
-          include ActiveRecord::Acts::Likeable::InstanceMethods
+# MovrActsAsLikable
+module Adventtec
+    module Acts #:nodoc:
+        module Likable #:nodoc:
+            
+            def self.included(base)
+                base.extend ClassMethods
+            end
+            
+            module ClassMethods
+                def acts_as_likable
+                    has_many :likes, :as => :likable, :dependent => :destroy
+                    include Adventtec::Acts::Likable::InstanceMethods
+                    extend Adventtec::Acts::Likable::SingletonMethods
+                end
+            end
+            
+            # This module contains class methods
+            module SingletonMethods
+                def find_likes_cast_by_user(user)
+                    likable = ActiveRecord::Base.send(:class_name_of_active_record_descendant, self).to_s
+                    Like.find(:all,
+                        :conditions => ["user_id = ? likable_type = ?", user.id, likable],
+                        :order => "created_at DESC"
+                    )
+                end
+            end
+            
+            # This module contains instance methods
+            module InstanceMethods
+                
+                # Same as likable.likes.size
+                def likes_count
+                    self.likes.count
+                end
+                
+                def users_who_liked
+                    self.likes.find(:all, :include => [:user]).map(&:user) 
+                end
+                
+                def liked_by_user?(user = nil)
+                  if not user
+                    user = current_user
+                  end
+                  user && self.likes.first(:conditions => {:user_id => user.id})
+                end
+            end
         end
-      end
-
-      # This module contains instance methods
-      module InstanceMethods
-        # Helper method that associates the Liking with the Object
-        def add_liking(liking)
-          likings << liking
-        end
-
-        # Helper method to calculate the total likes for this Likeable
-        def likes_count
-          likes = Liking.find(
-            :all,
-            :conditions => ["likeable_id = ? AND likeable_type = ? AND liking = TRUE",id, self.type.name])
-          likes.size
-        end
-
-        # Helper method to calculate the total dislikes for this Likeable
-        def dislikes_count
-          likes = Liking.find(
-            :all,
-            :conditions => ["likeable_id = ? AND likeable_type = ? AND liking = FALSE",id, self.type.name])
-          likes.size
-        end
-
-        # Helper method to calculate the total likings for this Likeable
-        def likings_count
-          self.likings.size
-        end
-
-        # Helper method to calculate the percentage of likes to dislikes for this Likeable
-        def likes_percentage
-          (likes_count*100)/likings_count
-        end
-
-        # Helper method to calculate the percentage of dislikes to likes for this Likeable
-        def dislikes_percentage
-          (dislikes_count*100)/likings_count
-        end
-
-        # Helper method to see if a user already liked this likeable
-        def liked_by_user?(user)
-          rtn = false
-          if user
-            self.likings.each { |l|
-              rtn = true if user.id == l.user_id
-            }
-          end
-          rtn
-        end
-      end
     end
-  end
 end
--- movr_acts_as_likeable.gemspec
+++ movr_acts_as_likeable.gemspec
@@ -5,7 +5,7 @@
 
 Gem::Specification.new do |s|
   s.name = %q{movr_acts_as_likeable}
-  s.version = "0.1.0"
+  s.version = "0.1.1"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Daryl Moulder, https://github.com/umerfarooq"]
